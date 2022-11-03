30dbd5e90d5babf490485485b3fd6d043b87d37b
rtj99 <ryantjena@gmail.com>
2022-11-03 12:59:08 +0200

    edits to profile page

---
+++ src/components/Filters.jsx
@@ -0,0 +1,52 @@
+/*
+  This example requires some changes to your config:
+  
+  ```
+  // tailwind.config.js
+  module.exports = {
+    // ...
+    plugins: [
+      // ...
+      require('@tailwindcss/forms'),
+    ],
+  }
+  ```
+*/
+const notificationMethods = [
+  { id: "minted", title: "Minted" },
+  { id: "bids", title: "Bids" },
+  { id: "liked", title: "Liked" },
+  { id: "followings", title: "Followings" },
+];
+
+export default function Example() {
+  return (
+    <div className="my-5 text-gray-900 dark:text-white ">
+      <label className="text-base font-medium text-gray-900 dark:text-white my-5">
+        Filters
+      </label>
+
+      <fieldset className="mt-4">
+        <div className="space-y-4 sm:flex sm:items-center sm:space-y-0 sm:space-x-10">
+          {notificationMethods.map((notificationMethod) => (
+            <div key={notificationMethod.id} className="flex items-center">
+              <input
+                id={notificationMethod.id}
+                name="notification-method"
+                type="radio"
+                defaultChecked={notificationMethod.id === "email"}
+                className="h-4 w-4 border-gray-300 text-indigo-600 focus:ring-indigo-500"
+              />
+              <label
+                htmlFor={notificationMethod.id}
+                className="ml-3 block text-sm font-medium text-gray-900 dark:text-white"
+              >
+                {notificationMethod.title}
+              </label>
+            </div>
+          ))}
+        </div>
+      </fieldset>
+    </div>
+  );
+}
--- src/pages/profile.jsx
+++ src/pages/profile.jsx
@@ -3,9 +3,10 @@ import { BsImage } from "react-icons/bs";
 import { FaUser } from "react-icons/fa";
 import { Layout } from "../components/Layout";
 import { FormControl, FormLabel, Stack, Switch } from "@chakra-ui/react";
-import { Radio, RadioGroup } from "@chakra-ui/react";
+import Filters from "../components/Filters";
 import { Tabs, TabList, TabPanels, Tab, TabPanel } from "@chakra-ui/react";
 import ProfileCards from "../components/ProfileCards";
+import { useRouter } from "next/router";
 function Profile() {
   const [value, setValue] = React.useState("1");
   // make a preview of the image
@@ -81,6 +82,7 @@ function Profile() {
       url: "The Centaurus ",
       imageSrc: "/images/rock.png",
       price: "5.0",
+      followers: "5",
     },
     {
       id: 3,
@@ -88,6 +90,7 @@ function Profile() {
       url: "The Centaurus ",
       imageSrc: "/images/buffalosoldier01.jpeg",
       price: "5.0",
+      followers: "9",
     },
     {
       id: 4,
@@ -95,6 +98,7 @@ function Profile() {
       url: "The Centaurus ",
       imageSrc: "/images/SCOTIA.png",
       price: "5.0",
+      followers: "5",
     },
 
     {
@@ -103,6 +107,7 @@ function Profile() {
       url: "The Centaurus ",
       imageSrc: "/images/cloud.jpeg",
       price: "5.0",
+      followers: "8",
     },
   ];
   const liked = [
@@ -112,6 +117,7 @@ function Profile() {
       url: "The Centaurus ",
       imageSrc: "/images/1.png",
       price: "5.0",
+      followers: "8",
     },
     {
       id: 2,
@@ -119,6 +125,7 @@ function Profile() {
       url: "The Centaurus ",
       imageSrc: "/images/rock.png",
       price: "5.0",
+      followers: "8",
     },
     {
       id: 3,
@@ -126,6 +133,7 @@ function Profile() {
       url: "The Centaurus ",
       imageSrc: "/images/buffalosoldier01.jpeg",
       price: "5.0",
+      followers: "8",
     },
     {
       id: 4,
@@ -142,6 +150,15 @@ function Profile() {
       url: "The Centaurus ",
       imageSrc: "/images/buffalosoldier01.jpeg",
       price: "5.0",
+      followers: "8",
+    },
+    {
+      id: 4,
+      name: "Buffaosoldier01 / 5 of 5",
+      url: "The Centaurus ",
+      imageSrc: "/images/buffalosoldier01.jpeg",
+      price: "5.0",
+      followers: "8",
     },
     {
       id: 4,
@@ -149,6 +166,7 @@ function Profile() {
       url: "The Centaurus ",
       imageSrc: "/images/SCOTIA.png",
       price: "5.0",
+      followers: "20",
     },
 
     {
@@ -157,6 +175,7 @@ function Profile() {
       url: "The Centaurus ",
       imageSrc: "/images/rock2.jpeg",
       price: "5.0",
+      followers: "3",
     },
     {
       id: 8,
@@ -164,8 +183,10 @@ function Profile() {
       url: "The Centaurus ",
       imageSrc: "/images/cloud.jpeg",
       price: "5.0",
+      followers: "8",
     },
   ];
+  const router = useRouter();
   return (
     <Layout>
       <div className="z-20 px-8 md:px-15 lg:px-20 pt-10 lg:pt-32">
@@ -202,8 +223,18 @@ function Profile() {
                   <Tab>Collectibles</Tab>
                   <Tab>Liked</Tab>
                   <Tab>Activity</Tab>
-                  <Tab>Following</Tab>
-                  <Tab>Followers</Tab>
+                  <Tab className="relative">
+                    Following
+                    <div className="absolute top-0 right-0 dark:bg-white bg-[#f1f1f1] text-black  rounded p-1 ">
+                      <p className="text-xs">{collection.length}</p>
+                    </div>
+                  </Tab>
+                  <Tab className="relative">
+                    Followers
+                    <div className="absolute top-0 right-0 dark:bg-white bg-[#f1f1f1] text-black  rounded p-1 ">
+                      <p className="text-xs">{onSale.length}</p>
+                    </div>
+                  </Tab>
                 </TabList>
 
                 <TabPanels>
@@ -220,23 +251,53 @@ function Profile() {
                     <ProfileCards products={onSale} />
                   </TabPanel>
                   <TabPanel>
-                    <ProfileCards products={collection} />
-                  </TabPanel>
-                  <TabPanel>
-                    {products.map((product, index) => (
+                    <Filters />
+                    {collection.map((product, index) => (
                       <div
                         key={index}
                         className="w-full rounded-2xl bg-white mb-3 border dark:bg-[#2d203c] dark:border-none p-5 grid grid-cols-2"
                       >
                         <div className="flex items-center">
                           <img
+                            onClick={() => router.push("/profile")}
                             src={product.imageSrc}
-                            className="h-10 w-10 object-cover rounded-full mr-5"
+                            className="h-32 w-32 object-cover rounded-2xl mr-5 cursor-pointer"
+                            alt=""
+                          />
+                          <div>
+                            <p className=" font-semibold text-lg dark:text-white ">
+                              {product.name}
+                            </p>
+                            <p className="text-sm font-medium my-2 dark:text-white">
+                              liked by @Shaker Sangam
+                            </p>
+                            <p className="text-sm font-medium dark:text-white">
+                              2 months ago
+                            </p>
+                          </div>
+                        </div>
+                      </div>
+                    ))}
+                  </TabPanel>
+                  <TabPanel>
+                    {collection.map((product, index) => (
+                      <div
+                        key={index}
+                        className="w-full rounded-2xl bg-white mb-3 border dark:bg-[#2d203c] dark:border-none p-5 grid grid-cols-2"
+                      >
+                        <div className="flex items-center">
+                          <img
+                            onClick={() => router.push("/profile")}
+                            src={product.imageSrc}
+                            className="h-10 w-10 object-cover rounded-full mr-5 cursor-pointer"
                             alt=""
                           />
                           <div>
                             <p className="text-sm font-medium dark:text-white">
                               {product.url}
+                            </p>
+                            <p className="text-sm font-medium dark:text-white">
+                              {product.followers} followers
                             </p>
                           </div>
                         </div>
@@ -257,20 +318,24 @@ function Profile() {
                       >
                         <div className="flex items-center">
                           <img
+                            onClick={() => router.push("/profile")}
                             src={product.imageSrc}
-                            className="h-10 w-10 object-cover rounded-full mr-5"
+                            className="h-10 w-10 object-cover rounded-full mr-5 cursor-pointer"
                             alt=""
                           />
                           <div>
                             <p className="text-sm font-medium dark:text-white">
                               {product.url}
                             </p>
+                            <p className="text-sm font-medium dark:text-white">
+                              {product.followers} followers
+                            </p>
                           </div>
                         </div>
 
                         <div className="flex items-center justify-end">
                           <button className="bg-[#F5F5F5] dark:bg-[#18111f] dark:text-white text-[#413A5A] px-5 py-2 rounded-xl font-medium">
-                            Unfollow
+                            Follow
                           </button>
                         </div>
                       </div>
